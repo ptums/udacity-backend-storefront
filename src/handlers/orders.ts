9fa2e81ed86d62240a2336191d24328dc47c8c7d
ptums <ptumulty923@gmail.com>
2023-04-30 20:34:00 -0400

    added create order route

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -14,9 +14,15 @@ const userOrders = async (req: Request, res: Response) => {
   res.json(order);
 };
 
+const create = async (req: Request, res: Response) => {
+  const order = await store.create(req.body);
+
+  res.json(order);
+};
 const orderRoutes = (app: express.Application) => {
   app.get('/orders/:status/:id', authMiddleware, orderStatus);
   app.get('/orders/:id', authMiddleware, userOrders);
+  app.post('/orders', create);
 };
 
 export default orderRoutes;
